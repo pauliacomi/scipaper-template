87574145915444fbecf4edeb8daf10a8f0de3377
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-09-04 18:55:07 +0200

    prepending figure folder

--- scripts/pd-image-filter.lua
+++ scripts/pd-image-filter.lua
@@ -3,6 +3,9 @@ if FORMAT:match 'markdown' then
   function Image(elem)
     -- Make sure extension is .png
     elem.src = elem.src:gsub("%.pdf", ".png")
+
+    -- Prepend figure directory
+    elem.src = "figs/" .. elem.src
     return elem
   end
 
