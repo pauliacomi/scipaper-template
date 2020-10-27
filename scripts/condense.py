4256644b885fdc66899a4feb0c0dccbda0f716d1
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-10-27 18:08:04 +0100

    python script changes

--- scripts/condense.py
+++ scripts/condense.py
@@ -11,12 +11,15 @@ import re
 STYLE_RE = re.compile(r"\\def\\style{\d}\s")  # style statement + newline
 INPUT_RE = re.compile(r"\\input{.*}")  # an input statement
 ARGUMENT_RE = re.compile(r"(?<={)[\s\S]*(?=})")  # any latex argument
-IF_RE = re.compile(r"\\if[\s\S]*?\\fi")  # any if/fi section
+IF_RE = re.compile(r"\\if[^def][\s\S]*?\\fi")  # any if/fi section not ifdef
 ELSE_RE = re.compile(r"\\else[\s\S]*?\\fi")  # any else/fi section
 IF_ARG_RE = re.compile(r"(?<=\\if\\style)\d")  # the style if comparator
 IF_ARG_STRIP = re.compile(r"\\if\\\w*\s*|\\fi")  # the if/fi edges
 ELSE_ARG_STRIP = re.compile(r"\\else|\\fi")  # the else/fi edges
 META_RE = re.compile(r"% !TEX .*")  # tex meta commands
+
+IFDEF_RE = re.compile(r"\\ifdef")  # any ifdefined/fi section
+NEWCOMM_RE = re.compile(r"\\newcommand")  # any ifdefined/fi section
 
 style_d = {
     '0': 'pi',
@@ -44,13 +47,28 @@ def get_style(text):
     return style, text
 
 
-def sort_ifs(text, style):
-    """Deal with any if/else/fi flags."""
+def find_close_bracket(string):
+    """finds the closing of a scope, taking into account nesting"""
+    nest = 1
+    for ind, char in enumerate(string):
+        if char == '{':
+            nest += 1
+        elif char == '}':
+            nest -= 1
+        if nest == 0:
+            return ind
+
+
+def sort_style_ifs(text, style):
+    r"""
+    Deal with any if/else/fi flags.
+    The if/else types to replace are only '\if\style'
+    """
     def repl_ifs(if_section):
         if_text = if_section.group()
-        if_style = IF_ARG_RE.search(if_text)
+        if_style = IF_ARG_RE.search(if_text)  # find if in group
         if not if_style:
-            raise Exception
+            raise Exception(if_text)
         else_match = ELSE_RE.search(if_text)
         if else_match:
             if_text = if_text[0:else_match.start()]
@@ -63,6 +81,43 @@ def sort_ifs(text, style):
             return ""
 
     return IF_RE.sub(repl_ifs, text)
+
+
+def sort_other_ifs(text):
+    r"""
+    Deal with other if flags.
+    # The types to replace are either \ifdef or \if@switch
+    """
+    def repl_ifswitch(if_section):
+        if_text = if_section.group()
+        if if_text.startswith("\\if@switch"):
+            if text.startswith("% Master SI"):
+                return ""
+            else_match = ELSE_RE.search(if_text)
+            if_text = if_text[0:else_match.start()]
+            else_text = else_match.group()
+            return ELSE_ARG_STRIP.sub("", else_text)
+        else:
+            raise Exception
+
+    text = IF_RE.sub(repl_ifswitch, text)
+
+    hit = IFDEF_RE.search(text)
+    while hit:
+        arg1_o = hit.end() + 1
+        arg1_e = arg1_o + find_close_bracket(text[arg1_o:])
+        arg2_o = arg1_e + 2
+        arg2_e = arg2_o + find_close_bracket(text[arg2_o:])
+        arg3_o = arg2_e + 2
+        arg3_e = arg3_o + find_close_bracket(text[arg3_o:])
+
+        if text.find(f"\\newcommand{{{text[arg1_o:arg1_e]}}}") != -1:
+            text = text[:hit.start()] + text[arg2_o:arg2_e] + text[arg3_e + 1:]
+        else:
+            text = text[:hit.start()] + text[arg3_o:arg3_e] + text[arg3_e + 1:]
+        hit = IFDEF_RE.search(text)
+
+    return text
 
 
 def expand(text):
@@ -79,10 +134,53 @@ def expand(text):
     return INPUT_RE.subn(repl_input, text)
 
 
+def expand_meta(text):
+
+    replaced = [
+        r'\pubauth', r'\pubaffil', r'\pubaddr', r'\orcid', r'\pubemail',
+        r'\pubkeywords', r'\pubSI', r'\pubtitle'
+    ]
+
+    replaced_vals = []
+    itertext = text
+    for hit in re.finditer(NEWCOMM_RE, itertext):
+        arg1_o = hit.end() + 1
+        arg1_e = arg1_o + find_close_bracket(itertext[arg1_o:])
+        arg2_o = arg1_e + 2
+        arg2_e = arg2_o + find_close_bracket(itertext[arg2_o:])
+
+        command = itertext[arg1_o:arg1_e]
+        value = itertext[arg2_o:arg2_e]
+
+        if any([command.startswith(com) for com in replaced]):
+            replaced_vals.append([command, value, hit.start(), arg2_e + 1])
+
+    text = ''.join([
+        chr for idx, chr in enumerate(text, 1) if not any(
+            strt_idx <= idx <= end_idx
+            for _, _, strt_idx, end_idx in replaced_vals
+        )
+    ])
+    for val in replaced_vals:
+        text = text.replace(val[0], val[1])
+
+    return text
+
+
 def clean(text, style):
 
     # remove meta commands
     text = META_RE.sub("", text)
+
+    # remove advanced tex lines
+    text = re.sub(r"\\makeatletter", r'', text)
+    text = re.sub(r"\\makeatother", r'', text)
+
+    # pandoc unnecessary commands
+    text = text.replace(
+        r"\newenvironment{widefigure}{\begin{figure*}}{\end{figure*}}", ""
+    )
+    text = text.replace("widefigure", "figure*")
 
     # remove superfluous newlines
     text = re.sub(r'\n\s*\n', r'\n\n', text)
@@ -99,22 +197,29 @@ def clean(text, style):
 
 
 def process_tex(target):
+    """Process a tex file to return it to a simple 'one file' format."""
     with target.open() as file:
 
-        # file contents
+        # get file contents
         filetext = file.read()
 
-        # style
+        # get style
         style, filetext = get_style(filetext)
 
-        # if/else processing
+        # style if/else processing
         if style:
-            filetext = sort_ifs(filetext, style)
+            filetext = sort_style_ifs(filetext, style)
 
         # input expansion
         expansions = 1
         while expansions > 0:
             filetext, expansions = expand(filetext)
+
+        # second if/else processing
+        filetext = sort_other_ifs(filetext)
+
+        # replace metadata commands
+        filetext = expand_meta(filetext)
 
         # cleaning
         filetext = clean(filetext, style)
@@ -142,14 +247,16 @@ def copy_files(source, target, style):
 
 def main(source_dir, output_dir='./processed/'):
 
+    # define source and output dirs
     source_dir = pathlib.Path(source_dir)
     output_dir = pathlib.Path(output_dir)
 
+    # create and clean output dir
     if output_dir.exists():
         shutil.rmtree(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # style
+    # the manuscript style (own, RCS, etc)
     man_style = None
 
     # condense the manuscript files
@@ -175,5 +282,4 @@ if __name__ == "__main__":
     arguments = docopt.docopt(__doc__)
     directory = arguments["<directory>"
                           ] or pathlib.Path(__file__).parent.parent
-    # output_dir = arguments["--output_dir"] or None
     main(directory)
